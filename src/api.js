0d6dffe44e8f9f59e4d80e356e96154b0131accc
Pavan Gurrala <pavankumarg987@gmail.com>
2024-11-26 13:02:17 +0000

    changed apis

--- src/api.js
+++ src/api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const API_BASE_URL = "https://fc4fxqq0dd.execute-api.us-east-1.amazonaws.com"; // Replace with your API Gateway URL
 
 export const getUsers = () => axios.get(`${API_BASE_URL}/users`);
-export const createUser = (user) => axios.post(`${API_BASE_URL}/users`, user);
-export const updateUser = (user) => axios.put(`${API_BASE_URL}/users`, user);
+export const createUser = (user) => axios.put(`${API_BASE_URL}/users`, user);
+export const updateUser = (user) => axios.patch(`${API_BASE_URL}/users`, user);
 export const deleteUser = (id) => axios.delete(`${API_BASE_URL}/users/${id}`);
